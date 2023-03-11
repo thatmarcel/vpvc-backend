ab886c66b54b83b4a51cb8a3bcd6d2e6c0d1f992
thatmarcel <contact@thatmarcel.com>
2023-03-11 13:15:38 +0100

    Increase participant states update frequency (every 500ms -> every 200ms)

--- VPVC-Backend/Config.cs
+++ VPVC-Backend/Config.cs
@@ -5,5 +5,5 @@ public static class Config {
     public static readonly int maxParticipantsPerTeam = 5;
     public static readonly int maxParticipantsPerParty = maxParticipantsPerTeam * teamCount;
 
-    public static readonly TimeSpan participantStatesUpdateInterval = TimeSpan.FromSeconds(0.5);
+    public static readonly TimeSpan participantStatesUpdateInterval = TimeSpan.FromSeconds(0.2);
 }
